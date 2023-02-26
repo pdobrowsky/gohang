b647605283ee8ff3c08ff95a25a47ab9286473c1
Paul Dobrowsky <pdbrosky@gmail.com>
2023-02-26 18:55:03 -0500

    remove posts from user.html

--- app/routes.py
+++ app/routes.py
@@ -74,10 +74,9 @@ def register():
 @login_required
 def user(username):
     user = User.query.filter_by(username=username).first_or_404()
-    posts = user.posts.order_by(Post.timestamp.desc())
 
     form= EmptyForm()
-    return render_template('user.html', user=user, posts=posts, form=form)
+    return render_template('user.html', user=user, form=form)
 
 @app.route('/edit_profile', methods=['GET', 'POST'])
 @login_required
