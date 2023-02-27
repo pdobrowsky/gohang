e5b12292d6ceb87b3cede7965e10d4471209b180
Paul Dobrowsky <pdbrosky@gmail.com>
2023-02-26 19:16:23 -0500

    remove explore route

--- app/routes.py
+++ app/routes.py
@@ -135,16 +135,6 @@ def unfollow(username):
 
     return redirect(url_for('user', username=username))
 
-@app.route('/explore')
-@login_required
-def explore():
-    page = request.args.get('page', 1, type=int)
-    posts = Post.query.order_by(Post.timestamp.desc()).paginate(page=page, per_page=app.config['POSTS_PER_PAGE'], error_out=False)
-    next_url = url_for('index', page=posts.next_num) if posts.has_next else None
-    prev_url = url_for('index', page=posts.prev_num) if posts.has_prev else None
-
-    return render_template('index.html', title='Explore', posts=posts.items, next_url=next_url, prev_url=prev_url)
-
 @app.route('/reset_password_request', methods=['GET', 'POST'])
 def reset_password_request():
     if current_user.is_authenticated:
