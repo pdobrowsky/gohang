8497627094a7075fa67ee78e8707d189a208c1a5
Paul Dobrowsky <pdbrosky@gmail.com>
2023-02-26 19:05:58 -0500

    remove about me from routes, forms, html

--- app/forms.py
+++ app/forms.py
@@ -30,7 +30,6 @@ class RegistrationForm(FlaskForm):
 
 class EditProfileForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired()])
-    about_me = TextAreaField('About me', validators=[Length(min=0, max=140)])
     submit = SubmitField('Submit')
 
     def __init__(self, original_username, *args, **kwargs):
