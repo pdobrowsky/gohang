82d02701fa9f51e076fef694be852ab85a5b55d9
Paul Dobrowsky <pdbrosky@gmail.com>
2023-02-26 19:20:21 -0500

    remove follow/unfollow from routes and html

--- app/routes.py
+++ app/routes.py
@@ -95,46 +95,6 @@ def edit_profile():
 
     return render_template('edit_profile.html', title='Edit Profile', form=form)
 
-@app.route('/follow/<username>', methods=['POST'])
-@login_required
-def follow(username):
-    form= EmptyForm()
-    user = User.query.filter_by(username=username).first()
-
-    if user is None:
-        flash('User {} not found.'.format(username))
-        return redirect(url_for('index'))
-    if user == current_user:
-        flash('You can\'t follow yourself!')
-        return redirect(url_for('user', username=username))
-
-    current_user.follow(user)
-    db.session.commit()
-
-    flash('You are now following {}'.format(username))
-
-    return redirect(url_for('user', username=username))
-
-@app.route('/unfollow/<username>', methods=['POST'])
-@login_required
-def unfollow(username):
-    form= EmptyForm()
-    user = User.query.filter_by(username=username).first()
-
-    if user is None:
-        flash('User {} not found.'.format(username))
-        return redirect(url_for('index'))
-    if user == current_user:
-        flash('You can\'t follow yourself!')
-        return redirect(url_for('user', username=username))
-
-    current_user.unfollow(user)
-    db.session.commit()
-    
-    flash('You stoppedfollowing {}'.format(username))
-
-    return redirect(url_for('user', username=username))
-
 @app.route('/reset_password_request', methods=['GET', 'POST'])
 def reset_password_request():
     if current_user.is_authenticated:
