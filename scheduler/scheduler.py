f875b4e672b218b87217876affe1b4ab56a3443e
Paul Dobrowsky <pdbrosky@gmail.com>
2023-03-01 09:10:01 -0500

    prep plan for scheduler

--- scheduler/scheduler.py
+++ scheduler/scheduler.py
@@ -1,7 +1,10 @@
-# needs to connect to sqlalchemy, get data down, pull it into pandas
+# needs to connect to sqlalchemy, 
+# get data down, 
+# pull it into pandas
+# clean up
 # needs to process schedules to create possible hangs
 # needs to write those results to new hangs table, update processed_at in schedule
-# it runs every day (because you can update your schedule), but on Sundays it does the week ahead, rest of week it only does current week (for now)
-# just create fake schedules by hand
+# it runs every day (because you can update your schedule), but on Sundays it does the week ahead, rest of week it only does current week (for now)=
+# ignore those already tried to schedule with since all on sms atm
 
 # going to need to have eddit schedule eventually
