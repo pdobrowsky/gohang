d298b7f041ab9ea87e3717de0cb4ef62d6315a29
Paul Dobrowsky <pdbrosky@gmail.com>
2023-04-17 16:38:31 -0400

    trying to fix migration again

--- app/models.py
+++ app/models.py
@@ -113,8 +113,6 @@ class Hang(db.Model):
     finalized_slot = db.Column(db.String(255))
     retry = db.Column(db.Boolean, index=True, default=False)
     reminded = db.Column(db.Boolean, index=True, default=False)
-    connect_type = db.Column(db.String(255), index=True)
-    test_field = db.Column(db.String(255))
 
     def __repr__(self):
         return '<Hang>'
